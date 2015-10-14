99ebc87719434986c0d9409783baef6dcb447850
Do Minh Hai <dominhhai@MacBook-Air.local>
2015-10-14 13:43:29 +0700

    Initial Commit

---
+++ Stars Field/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stars Field
+//
+//  Created by Do Minh Hai on 10/14/15.
+//  Copyright (c) 2015 Do Minh Hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Stars Field/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Stars Field
+//
+//  Created by Do Minh Hai on 10/14/15.
+//  Copyright (c) 2015 Do Minh Hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
