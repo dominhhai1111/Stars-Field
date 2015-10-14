800f0b2dd5e32f1cf4b008893c6c28b7cedba12b
Do Minh Hai <dominhhai@MacBook-Air.local>
2015-10-14 14:37:21 +0700

    first commit

--- Stars Field/ViewController.m
+++ Stars Field/ViewController.m
@@ -9,19 +9,65 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    UIImageView * red, *violet, *green, *brown ;
+    CGPoint center;
+    CGFloat deltaX ;
+    CGFloat deltaY ;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    CGSize size = self.view.bounds.size ;
+    deltaX = 100 ;
+    deltaY = 100;
+    center = CGPointMake(size.width * 0.5, (size.height - 60) * 0.5);
+    red    = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"red"]];
+    violet = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"violet"]];
+    green  = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"green"]];
+    brown  = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"brown"]];
+    [self vitri];
+    [self.view addSubview:red];
+    [self.view addSubview:violet];
+    [self.view addSubview:brown];
+    [self.view addSubview:green];
+    [self animate];
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void) vitri
+{
+    red.center = center;
+    violet.center = center;
+    green.center = center;
+    brown.center = center;
 }
-
+-(void) animate
+{
+    [UIView animateWithDuration:1 animations:^{
+        brown.center = CGPointMake(center.x - deltaX, center.y - deltaY);
+        red.center = CGPointMake(center.x + deltaX, center.y + deltaY);
+        green.center = CGPointMake(center.x + deltaX, center.y - deltaY);
+        violet.center = CGPointMake(center.x - deltaX, center.y + deltaY)  ;
+    }
+                     completion:^(BOOL finished){
+        [UIView animateWithDuration:1 animations:^{
+            brown.center = CGPointMake(center.x - deltaX, center.y);
+            green.center = CGPointMake(center.x, center.y - deltaY);
+            red.center = CGPointMake(center.x + deltaX, center.y);
+            violet.center = CGPointMake(center.x, center.y + deltaY);
+        }
+                         completion:^(BOOL finished){
+            [UIView animateWithDuration:3 animations:^{
+                [self vitri];
+            }
+                             completion:^(BOOL finished){
+                                 [self animate];
+                             }];
+        }];
+    }];
+}
 @end
